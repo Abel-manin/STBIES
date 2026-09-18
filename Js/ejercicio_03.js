a74b70b67ba2f18be72dd6bb2b13cd12f565ed9e
mainbel <adele.adel.saenz@gmail.com>
2026-09-18 08:36:29 -0500

    actuali

---
+++ Js/ejercicio_03.js
@@ -0,0 +1,16 @@
+let usuario = Witen;
+let Velocidadkilometos = 120;
+let LitrosCombustible =  10;
+
+let combustbleConsumido = LitrosCombustible - Velocidadkilometos * 0.10
+let DistanciaTotal = Velocidadkilometos * LitrosCombustible * combustbleConsumido
+
+console.log("Usuario"), usuario ,("Ingrese su distancia recorrida:"), DistanciaTotal;
+console.log("Usuario"); usuario ,("Ingrese su combustible inicial"), LitrosCombustible;
+console.log("Usuario"); usuario ,("Ingrese la velocidad a la que iba"),Velocidadkilometos ;
+console.log("Su conbustible consumido fue..."), combustbleConsumido;
+console.log("Su distancia total fue..."), DistanciaTotal;
+
+
+
+
---
+++ Js/ejercicio_04.js
@@ -0,0 +1,16 @@
+let empleado = "Jhon Doe"
+let horasTrabajadas = 40;
+let tarifa = 50;
+
+let salario = horasTrabajadas * tarifaHoras
+const impuesto = 0.10 * salarioBruto;
+let salarioNeto = salarioBruto * impuesto;
+
+console.log("::::::BOLETA DE PAGO::::::");
+console.log("Nombre del empleado"),empleado ; 
+console.log("Horas trabajadas"),horasTrabajadas ;
+console.log("Tarifa por Horas S/"),horasTrabajadas ;
+console.log("Salario Bruto S/"),salarioBruto ;
+console.log("Impuesto S/"),impuesto ;
+console.log("Salario Neto S/"),salarioNeto ;
+
