3b4c69bb157ef8957f5499f6e089fcc52d1e34a3
mainbel <adele.adel.saenz@gmail.com>
2026-09-18 20:10:16 -0500

    V2

--- Js/Alt-ejercicio_01.js
+++ Js/Alt-ejercicio_01.js
@@ -1,6 +1,6 @@
 let numero1 = 33;
 let numero2 = 12;
-
+ 
 const numeroMayor = numero1 > numero2;
 const numeroMenor = numero2 < numero1;
 const numeroMayorC = numero2 > numero1;
@@ -11,4 +11,3 @@ console.log("Comparación... 33 sera mayor que 12", numeroMayor);
 console.log("Comparación... 12 sera menor que 33", numeroMenor);
 console.log("Comparación...33 sera menor que 12", numeroMayorC);
 console.log("Comparación... 12 sera mayor que 33", numeroMenorC);
-
---
+++ Js/Alt-ejercicio_02.js
@@ -0,0 +1,19 @@
+let comp1 = 93;
+let comp2 = 71;
+let comp3 = 69;
+let comp4 = 34;
+
+let numMayor = comp1 > comp2 && comp2 > comp3 && comp3 > comp4;
+let numMenor = comp1 < comp2 && comp2 < comp3 && num3 < comp4;
+
+console.log("Comparativa de numeros");
+console.log("Los numeros 71,34,69 y 93 seran los escaneados");
+
+if (comp1 > comp2 && comp2 > comp3 && comp3 > comp4) {  
+    numMayor = comp1;
+    numMenor = comp4;
+    console.log("El numero mayo es", numMayor, "y el menor termina siendo", numMenor);
+} else {
+   (comp1 < comp2 && comp2 < comp3 && num3 < comp4)
+   console.log("Error.. Fallo en la operacion..");
+}
