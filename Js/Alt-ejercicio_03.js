1661a5206b444a98c2942cf597fcb21c30ac52ae
mainbel <adele.adel.saenz@gmail.com>
2026-09-20 09:58:00 -0500

    Final de 20

---
+++ Js/Alt-ejercicio_03.js
@@ -0,0 +1,23 @@
+let numeroMcr = 28;
+let numeroSoad = 19;
+
+let numeroSuma = numeroMcr + numeroSoad;
+let numeroMultiplicacion = numeroMcr * numeroSoad;
+
+
+console.log("::::Calculo  exagerado::::")
+console.log("Numero enteros")
+console.log("Son bastantes por no decir que son infinitos")
+console.log("Un concepto tan poderoso como el infinito dirige a pensamientos que jámas debieron pensarse");
+console.log("Situaciones que hacen pensar en lo bueno, lo malo o el porvenir");
+console.log("El infinito es un concepto igual de poderoso como el 0");
+console.log("La nada da a pensar en algo a pesar de ser absolutamente nada");
+console.log("Un cero sigue siendo algo a pesar de significar absolutamente nada");
+console.log("Pero a nadie le a de importar lo anteriormente escrito, ya que este mensaje es solo para sumar y multiplicar");
+console.log("Los numero que emos de operar son el", numeroMcr, "y él", numeroSoad);
+console.log("Estos números serán sumados y multiplicados");
+console.log("La suma de los números (", numeroMcr, numeroSoad ,")")
+console.log("El resultado llegaría a ser =", numeroSuma);
+console.log("Por otro lado");
+console.log("La multiplicaión de los números (", numeroMcr, numeroSoad, ")");
+console.log("El resultado llegaría a ser =", numeroMultiplicacion);
